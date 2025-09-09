1b91072d798a9df5268ccd0333aac2c220d08134
marce <mchamorro@fiveit.com.ar>
2025-09-08 22:04:57 -0300

    first commit

---
+++ src/main/java/com/unrn/catalogo/catalogo_service/CatalogoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unrn.catalogo.catalogo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoServiceApplication.class, args);
+	}
+
+}
